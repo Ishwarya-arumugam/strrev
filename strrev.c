0b79d6fea4331ab5a0360b57aed086071f30e01f
Ishwarya-arumugam <ishwaryaa44@gmail.com>
2016-07-02 11:07:25 +0530

    Create strrev.c

---
+++ strrev.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+char a[50];
+clrscr();
+scanf("%s",a);
+//b=strlen(a);
+//for(i=b;i>=0;i--)
+{
+printf("%s",strrev(a));
+}
+getch();
+}
